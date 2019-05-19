1d6a76a222fa6d28b5deaea850e0846b1e74cdbd
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-19 21:49:38 +0700

    weather 2

--- index.js
+++ index.js
@@ -4,6 +4,22 @@ const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'Здравствуй! Пожалуйста, покажи, где ты находишься.')
-    
 })
+
+bot.onText(/^\/place_order/, function (msg, match) {
+    var option = {
+        "parse_mode": "Markdown",
+        "reply_markup": {
+            "one_time_keyboard": true,
+            "keyboard": [[{
+                text: "My phone number",
+                request_contact: true
+            }], ["Cancel"]]
+        }
+    };
+    bot.sendMessage(msg.chat.id, "How can we contact you?", option).then(() => {
+        // handle user phone
+    })
+
+});
 
