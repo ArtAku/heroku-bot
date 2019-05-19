95b1b561db5a01d4f1420ce9c7c550f95f0f2f7e
Artem Akulov <akulov.artem.1997@gmail.com>
2019-05-19 21:24:12 +0700

    weather messege 1

--- index.js
+++ index.js
@@ -4,15 +4,6 @@ const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
     bot.sendMessage(msg.chat.id, 'Здравствуй! Пожалуйста, покажи, где ты находишься.')
-    var option = {
-        "parse_mode": "Markdown",
-        "reply_markup": {
-            "one_time_keyboard": true,
-            "keyboard": [[{
-                text: "Отправить мое местоположение",
-                request_contact: true
-            }] ["Неважно"]]
-        }
-    }
+    
 })
 
